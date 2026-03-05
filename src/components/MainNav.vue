e3fb76d2b73915ffcc1e3c324d648c859f84129a
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2026-03-05 08:02:45 -0300

    Section 9: Directives II

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,8 +1,9 @@
 <template>
   <header class="w-full text-sm">
-    <div class="fixed top-0 left-0 w-full h-16 bg-white">
-      <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
+      <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+
         <nav class="ml-12 h-full">
           <ul class="flex h-full list-none">
             <li v-for="menuItem in menuItens" :key="menuItem" class="ml-9 h-full first:ml-0">
@@ -10,20 +11,38 @@
             </li>
           </ul>
         </nav>
+
+        <div class="ml-auto flex h-full items-center">
+          <profile-image v-if="isLoggedIn" />
+          <action-button v-else @click="loginUser" />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from '@/components/ActionButton.vue'
+import ProfileImage from '@/components/ProfileImage.vue'
+
 export default {
   name: 'MainNav',
+  components: {
+    ActionButton,
+    ProfileImage,
+  },
   data() {
     return {
       company: 'Bobo Careers',
       url: 'https://careers.google.com',
       menuItens: ['Teams', 'Locations', 'Life at Bobo Corp', 'How we hire', 'Students', 'Jobs'],
+      isLoggedIn: false,
     }
+  },
+  methods: {
+    loginUser() {
+      this.isLoggedIn = true
+    },
   },
 }
 </script>
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,6 +1,7 @@
 import {render, screen} from '@testing-library/vue';
 import MainNav from '@/components/MainNav.vue';
 import { describe, expect, it } from 'vitest';
+import userEvent from "@testing-library/user-event";
 
 describe('MainNav', () => {
   it('displays company name', () => {
@@ -18,5 +19,26 @@ describe('MainNav', () => {
     expect(navigationMenuTexts).toEqual([
       'Teams', 'Locations', 'Life at Bobo Corp', 'How we hire', 'Students', 'Jobs',
     ]);
+  });
+
+    describe("when the user logs in", () => {
+    it("displays user profile picture", async () => {
+      render(MainNav);
+
+      let profileImage = screen.queryByRole("img", {
+        name: /user profile image/i,
+      });
+      expect(profileImage).not.toBeInTheDocument();
+
+      const loginButton = screen.getByRole("button", {
+        name: /sign in/i,
+      });
+      await userEvent.click(loginButton);
+
+      profileImage = screen.getByRole("img", {
+        name: /user profile image/i,
+      });
+      expect(profileImage).toBeInTheDocument();
+    });
   });
 });
